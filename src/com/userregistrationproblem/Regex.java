00b01816072be138f63d25a21d806db14f5d6050
sumi <sumipavib1302@gmail.com>
2023-01-29 17:46:46 +0530

    Added:FirstName Validation

--- src/com/userregistrationproblem/Regex.java
+++ src/com/userregistrationproblem/Regex.java
@@ -1,5 +1,19 @@
 package com.userregistrationproblem;
 
-public class Regex {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class Regex {
+	//Firstname validation
+    public  void validFirstName() {
+         Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,15}$");
+        Matcher matcher = pattern.matcher("Sumithra");
+        if (matcher.matches()){
+            System.out.println("valid");
+        }else {
+            System.out.println("invalid");
+        }
+        
+    }   
+    
 }
--- src/com/userregistrationproblem/RegexMain.java
+++ src/com/userregistrationproblem/RegexMain.java
@@ -1,5 +1,9 @@
 package com.userregistrationproblem;
 
 public class RegexMain {
-
+	public static void main(String args[]){
+        Regex regex = new Regex();
+        regex.validFirstName();
+        
+	}
 }
